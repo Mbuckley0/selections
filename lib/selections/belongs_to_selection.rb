3daccc8517a3496d7529ba432cd952bfba59cfa8
Mitchell Buckley <mitchell.buckley@tworedkites.com>
2020-05-28 10:36:11 +1000

    Remove predicate, scope and naming methods to improve performance

--- lib/selections/belongs_to_selection.rb
+++ lib/selections/belongs_to_selection.rb
@@ -24,63 +24,7 @@ module Selections
     # Note that this is only appropriate to use for system selection values that are known
     # at development time, and not to values that the users can edit in the live system.
     def belongs_to_selection(target, options = {})
-      system_code = options[:system_code]
-      predicates = !!options[:predicates]
-      scopes = !!options[:scopes]
-
-      belongs_to target, options.reject { |k, v| [:system_code, :scopes, :predicates].include?(k) }.merge(class_name: "Selection")
-
-      # The "selections" table may not exist during certain rake scenarios such as db:migrate or db:reset.
-      ActiveRecord::Base.connection_pool.with_connection(&:active?) rescue return
-      if ActiveRecord::VERSION::MAJOR > 4
-        table_exists = ActiveRecord::Base.connection.data_source_exists?(Selection.table_name)
-      else
-        table_exists = ActiveRecord::Base.connection.table_exists?(Selection.table_name)
-      end
-
-      if table_exists
-        prefix = self.name.downcase
-        parent = Selection.where(system_code: system_code).first || Selection.where(system_code: "#{prefix}_#{target}").first || Selection.where(system_code: target.to_s).first
-        target_id = "#{target}_id".to_sym
-        if parent
-          parent.children.each do |s|
-            if predicates
-              if system_code
-                method_name = "#{target}_#{s.system_code.to_s.gsub("#{target}_", '')}?".to_sym
-              else
-                method_name = "#{s.system_code.to_s}?".to_sym
-              end
-              class_eval do
-                define_method method_name do
-                  Array(send(target_id)).include?(s.id)
-                end
-              end
-
-              if scopes
-                if system_code
-                  scope_name = "#{target}_#{s.system_code.to_s.gsub("#{target}_", '')}".pluralize.to_sym
-                else
-                  scope_name = "#{s.system_code.to_s}".pluralize.to_sym
-                end
-
-                if ActiveRecord::VERSION::MAJOR >= 4
-                  scope scope_name, -> { where("#{target_id} = ?", s.id) }
-                else
-                  scope(scope_name, where("#{target_id} = ?", s.id))
-                end
-              end
-            end
-          end
-        end
-
-        class_eval do
-          define_method "#{target}_name" do
-            Selection.where(id: Array(self[target_id]).reject(&:blank?)).map(&:name).join(', ')
-          end
-        end
-
-        Selections::MethodMissingHelpers.add_method_missing_methods(self)
-      end
+      belongs_to target, options.reject { |k, v| [:system_code].include?(k) }.merge(class_name: "Selection")
     end
 
     ActiveSupport.on_load :active_record do
--- lib/selections/has_many_selections.rb
+++ lib/selections/has_many_selections.rb
@@ -24,75 +24,13 @@ module Selections
     # Note that this is only appropriate to use for system selection values that are known
     # at development time, and not to values that the users can edit in the live system.
     def has_many_selections(target, options = {})
-      system_code = options[:system_code]
-      predicates = !!options[:predicates]
-      scopes = !!options[:scopes]
       target_id = "#{target.to_s.singularize}_ids".to_sym
 
       if ActiveRecord::VERSION::MAJOR > 4
-        has_many target, options.reject { |k, v| [:system_code, :scopes, :predicates].include?(k) }.merge(class_name: 'Selection', primary_key: target_id, foreign_key: :id)
+        has_many target, options.reject { |k, v| [:system_code].include?(k) }.merge(class_name: 'Selection', primary_key: target_id, foreign_key: :id)
       else
-        has_many target, options.reject { |k, v| [:system_code, :scopes, :predicates].include?(k) }.merge(class_name: self.name, primary_key: target_id, foreign_key: :id)
+        has_many target, options.reject { |k, v| [:system_code].include?(k) }.merge(class_name: self.name, primary_key: target_id, foreign_key: :id)
       end
-
-      # The "selections" table may not exist during certain rake scenarios such as db:migrate or db:reset.
-      ActiveRecord::Base.connection_pool.with_connection(&:active?) rescue return
-      if ActiveRecord::VERSION::MAJOR > 4
-        table_exists = ActiveRecord::Base.connection.data_source_exists?(Selection.table_name)
-      else
-        table_exists = ActiveRecord::Base.connection.table_exists?(Selection.table_name)
-      end
-
-      if table_exists
-        prefix = self.name.downcase
-        parent = Selection.where(system_code: system_code).first || Selection.where(system_code: "#{prefix}_#{target.to_s.singularize}").first || Selection.where(system_code: target.to_s.singularize).first
-        if parent
-          parent.children.each do |s|
-            if predicates
-              if system_code
-                method_name = "#{target.to_s.singularize}_#{s.system_code.to_s.gsub("#{target.to_s.singularize}_", '')}?".to_sym
-              else
-                method_name = "#{s.system_code.to_s}?".to_sym
-              end
-              class_eval do
-                define_method method_name do
-                  Array(self[target_id]).map(&:to_s).include?(s.id.to_s)
-                end
-              end
-
-              if scopes
-                if system_code
-                  scope_name = "#{target.to_s.singularize}_#{s.system_code.to_s.gsub("#{target.to_s.singularize}_", '')}".pluralize.to_sym
-                else
-                  scope_name = "#{s.system_code.to_s}".pluralize.to_sym
-                end
-
-                if ActiveRecord::VERSION::MAJOR >= 4
-                  scope scope_name, -> { where("#{target_id} LIKE ?", "%#{s.id}%") }
-                else
-                  scope(scope_name, where("#{target_id} LIKE ?", "%#{s.id}%"))
-                end
-              end
-            end
-          end
-        end
-
-        class_eval do
-          define_method "#{target.to_s.singularize}_names" do
-            Selection.where(id: Array(self[target_id]).reject(&:blank?)).map(&:name).join(', ')
-          end
-
-          define_method "#{target_id}=" do |values|
-            if ActiveRecord::VERSION::MAJOR > 4
-              self[target_id] = values
-            else
-              self[target_id] = values.join(',')
-            end
-          end
-        end
-      end
-
-      Selections::MethodMissingHelpers.add_method_missing_methods(self)
     end
 
     ActiveSupport.on_load :active_record do
